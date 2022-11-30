488c7cd03c6b42763ab0c402006e52d0fd61abd1
GimmeDaLute <peter.m.edwards@outlook.com>
2022-11-30 11:54:42 +0000

    URL SHRTNR

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 
 export default function Home() {
+
+  const [longUrl, setLongUrl] = useState('')
+
+  function handleSubmit(e) {
+    e.preventDefault()
+  }
+
   return (
-    <h1>SH&#9988;RT&#9988;N&#9988;R</h1>
+    <div>
+      <h1>SH&#9988;RT&#9988;N&#9988;R</h1>
+      <div>
+        <form>
+          <label>
+            <input type="text" longurl="longurl" />
+            </label>
+            <input type="submit" value="SHRTN!" />
+        </form>
+      </div>
+    </div>
   )
 }
