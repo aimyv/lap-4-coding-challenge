825afa7642d0aa31465c634eb59935da7af4ceef
aimyv <av533@bath.ac.uk>
2022-11-30 17:00:29 +0000

    imported google font

--- client/src/layouts/Footer/index.js
+++ client/src/layouts/Footer/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div>
-      <p> <a href="https://github.com/aimyv/lap-4-coding-challenge">Project Repo</a></p>
+      <p> <a href="https://github.com/aimyv/lap-4-coding-challenge" target='_blank'>Project Repo</a></p>
     </div>
   )
 }
