87348426b7832669e2a8ba054c01be878e3ce694
aimyv <av533@bath.ac.uk>
2022-11-30 10:04:35 +0000

    edited post so that it returns the object

--- server/app.py
+++ server/app.py
@@ -56,7 +56,7 @@ def urls_handler():
                 id=count+1, long_path=uData["long_path"], short_path=code)
             db.session.add(new_url)
             db.session.commit()
-            return jsonify(uData), 201
+            return jsonify(list(new_url)), 201
 
 
 def getrandom():
