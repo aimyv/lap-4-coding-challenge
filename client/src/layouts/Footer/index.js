e94bea515e6f93346d04511fa7e384c44ae24443
aimyv <av533@bath.ac.uk>
2023-09-30 11:00:11 +0100

    changed footer to project backend

--- client/src/layouts/Footer/index.js
+++ client/src/layouts/Footer/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div>
-      <p id='footer'> <a href="https://github.com/aimyv/lap-4-coding-challenge" target='_blank'>Project Repo</a></p>
+      <p id='footer'> <a href="https://shrtnr.onrender.com/" target='_blank'>Project Backend</a></p>
     </div>
   )
 }
